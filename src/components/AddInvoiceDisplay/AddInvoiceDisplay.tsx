87ab3d04f099ebda212db3f0c942335e46ffa7c2
Alex Imre <100096476+AlexImre@users.noreply.github.com>
2023-04-25 16:54:57 +0100

    remove apikey

--- src/components/AddInvoiceDisplay/AddInvoiceDisplay.tsx
+++ src/components/AddInvoiceDisplay/AddInvoiceDisplay.tsx
@@ -81,49 +81,6 @@ export default function AddInvoiceDisplay() {
     }
   };
 
-  const sibEmail = async () => {
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        'api-key':
-          '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',
-      },
-      body: JSON.stringify({
-        sender: {
-          name: 'Mary from MyShop',
-          email: 'alexandre.imre@gmail.com',
-        },
-        to: [{ email: 'alexandre.imre@gmail.com', name: 'Jimmy' }],
-        htmlContent:
-          '<!DOCTYPE html> <html> <body> <h1>Confirm you email</h1> <p>Please confirm your email address by clicking on the link below</p> </body> </html>',
-        textContent:
-          'Please confirm your email address by clicking on the link https://text.domain.com',
-        subject: 'Login Email confirmation',
-        replyTo: { email: 'ann6533@example.com', name: 'Ann' },
-        // attachment: [
-        //   {
-        //     url: 'https://attachment.domain.com/myAttachmentFromUrl.jpg',
-        //     content: 'b3JkZXIucGRm',
-        //     name: 'myAttachment.png',
-        //   },
-        // ],
-        // headers: {
-        //   'sender.ip': '1.2.3.4',
-        //   'X-Mailin-custom': 'some_custom_header',
-        //   idempotencyKey: 'abc-123',
-        // },
-        templateId: 2,
-      }),
-    };
-
-    await fetch('https://api.sendinblue.com/v3/smtp/email', options)
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => console.error(err));
-  };
-
   const emailInvoice = async (invoice) => {
     console.log('made it to emailInvoice()');
     const emailedInvoice = await fetch('/api/send-mail', {
