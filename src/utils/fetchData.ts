a2f3c12498e9cab4670da957fbe2d84166ec31a8
Alex Imre <100096476+AlexImre@users.noreply.github.com>
2023-04-26 09:49:01 +0100

    wrap SSR fetches in try/catch

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,19 +1,23 @@
-export const fetchOrganisation = async ( email: string ) => {
+export const fetchOrganisation = async (email: string) => {
+  try {
     const fetchData = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getorganisation`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email }),
-        }
-      );
-      const orgData = await fetchData.json();
-      return orgData;
-}
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/getorganisation`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
+    const orgData = await fetchData.json();
+    return orgData;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const fetchInvoice = async ( invoiceId: string) => {
+export const fetchInvoice = async (invoiceId: string) => {
   const fetchData = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/view/${invoiceId}`,
     {
@@ -25,24 +29,30 @@ export const fetchInvoice = async ( invoiceId: string) => {
   );
   const invoiceData = await fetchData.json();
   return invoiceData;
-}
+};
 
 // does this not need to be encoded?!
-export const fetchInvoices = async ( organisationId: string ) => {
-  const fetchData = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/invoices/${encodeURIComponent(organisationId)}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-  const invoicesData = await fetchData.json();
-  return invoicesData;
-}
+export const fetchInvoices = async (organisationId: string) => {
+  try {
+    const fetchData = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/invoices/${encodeURIComponent(
+        organisationId
+      )}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const invoicesData = await fetchData.json();
+    return invoicesData;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const fetchInvoiceNumber = async ( organisationId: string ) => {
+export const fetchInvoiceNumber = async (organisationId: string) => {
   const fetchInvoiceNumber = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getinvoicenumbercount`,
     {
@@ -55,4 +65,4 @@ export const fetchInvoiceNumber = async ( organisationId: string ) => {
   );
   const invoiceNumber = await fetchInvoiceNumber.json();
   return invoiceNumber;
-}
+};
